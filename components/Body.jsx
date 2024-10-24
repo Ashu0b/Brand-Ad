a309ac90da302ff820917c1576b30c784aaef431
Ashis-Thorium <ashisbisoi05@gmail.com>
2024-10-24 15:20:57 +0530

    Updated

---
+++ components/Body.jsx
@@ -0,0 +1,26 @@
+
+const Body = () => {
+  return (
+    <main className="body container">
+      <div className="body-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+        <div >
+            <button className="body-btn">Shop Now</button>
+            <button className="_body-btn">Category</button>
+        </div>
+        <div className="eshop">
+            <p>Also Available On</p>
+            <div className="flip">
+                <img src="/images/flipkart.png" alt="" />
+                <img src="/images/amazon.png" alt="" />
+            </div>
+        </div>
+      </div>
+      <div className="body-image">
+      <img src="/images/shoe_image.png" alt="" />
+      </div>
+    </main>
+  );
+};
+export default Body;
---
+++ components/Navigation.jsx
@@ -0,0 +1,18 @@
+
+const Navigation=()=>{
+    return (
+        <nav className="container">
+        <div className="logo"><img src="images/image.png" alt="logo is loading" />  </div>
+        <ul>
+          <li href="#">MENU</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+        </ul>
+        <button>
+          LOGIN
+        </button>
+      </nav>
+    )
+}
+export default Navigation
